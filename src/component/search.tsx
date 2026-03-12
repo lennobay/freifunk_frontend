fcd78b319f88c45d8d69ee24b2ecedc79f921dc1
lennobay <lennard.e.haufe@outlook.com>
2026-03-12 23:27:53 +0100

    DEV_Version 1.1 (Search Implementation)
    DEV_Version 1.* is all Search Implementation of first iteration.
    
    There was already a first version to it and now i am going in the second one. The first was never published. None the less i can utilise parts of the logic from my first version.
    
    Basic creation of the search function on the map. No styles are "appended" to it. Multiple features need to be implemented in the future. Such as:
    1. Word part complition
    2. Clickable display on the map (I can use the already created logic from the old version, partly at least)
    3. Mobile and Desktop optimisation (generall display optimisation)
    
    All commit messages are in English. This commit message is written by a human.

--- src/component/search.tsx
+++ src/component/search.tsx
@@ -1,36 +1,48 @@
 import { useEffect, useState } from "react";
 
-export default function SearchComponent(){
-    const [searchquery, setsearchquery] = useState<String>("");
-    const [output, setoutput] = useState<String[]>([]);
-    useEffect(() => {
-        const getData = async () =>{
-            const searchurl = "http://localhost:8081/v1/search/" + searchquery;
-            const data = await fetch(searchurl);
-            const data_parsed = await data.json();
-           setoutput(data_parsed.query_output.total);
-            const temp: string[] =  [];
-            for(let i = 0; i <= data_parsed.query_output.total; i++){
-               
-                const temp_name = await  data_parsed.query_output.documents[i].value.name;
-                temp.push(temp_name);
-            }
-            setoutput(temp);
+export default function SearchComponent() {
+  const [searchquery, setsearchquery] = useState<String>("");
+  const [output, setoutput] = useState<String[]>([]);
+  useEffect(() => {
+    const getData = async () => {
+      const searchurl = "http://localhost:8081/v1/search/" + searchquery;
+
+      try {
+        const data = await fetch(searchurl);
+        const data_parsed = await data.json();
+        console.log(data_parsed);
+        console.log(data_parsed.query_output.documents[0].value.name);
+        setoutput(data_parsed.query_output.total);
+        const temp: string[] = [];
+        for (let i = 0; i < data_parsed.query_output.documents.length; i++) {
+          const temp_name =
+            await data_parsed.query_output.documents[i].value.name;
+          temp.push(temp_name);
         }
-        console.log(output);
+        setoutput(temp);
+      } catch (e) {
+        const temp = ["Nichts gefunden!"];
+        setoutput(temp);
+      }
+    };
+    console.log(output);
 
-        getData();
-    }, [searchquery])
-    return(
-        <>
-        <form>
-            <input onChange={(e) => setsearchquery(e.target.value)} placeholder="Wohnzimmer, Bühne..."/>
-        </form>
-
-        </>
-
-
-    );
-
-
+    getData();
+  }, [searchquery]);
+  return (
+    <>
+      <form >
+        <input
+          onChange={(e) => setsearchquery(e.target.value)}
+          placeholder="Wohnzimmer, Bühne..."
+        />
+      </form>
+      <div>
+        {" "}
+        {output.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    </>
+  );
 }
